ef0a6e11180d2dd24de672ea3ba672bead12cca5
ondicho <ondicho53@gmail.com>
2020-11-14 23:00:51 +0300

    uses butterknife to bind button view in mainactivity

--- app/src/main/java/com/moringa/favoriterecipe/MainActivity.java
+++ app/src/main/java/com/moringa/favoriterecipe/MainActivity.java
@@ -3,9 +3,13 @@ package com.moringa.favoriterecipe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    @Bindview ()
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
