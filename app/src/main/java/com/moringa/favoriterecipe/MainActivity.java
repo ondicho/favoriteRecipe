0aae5a758eca4d87d4244d93f5b7ff06d05895cf
ondicho <ondicho53@gmail.com>
2020-11-15 00:24:47 +0300

    implements navigation from main activity to search activity by the use of intents

--- app/src/main/java/com/moringa/favoriterecipe/MainActivity.java
+++ app/src/main/java/com/moringa/favoriterecipe/MainActivity.java
@@ -2,17 +2,29 @@ package com.moringa.favoriterecipe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener{
 
-    @Bindview ()
+    @BindView(R.id.findRecipeButton) Button mFindRecipeButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v==mFindRecipeButton){
+            Intent intent=new Intent(MainActivity.this,SearchActivity.class);
+            startActivity(intent);
+        }
     }
 }
