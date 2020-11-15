9ee4fda9518e6b46d0a423c55be384e974eaccb2
ondicho <ondicho53@gmail.com>
2020-11-16 01:37:29 +0300

    sets an onclick listener to display taoasts when individual list item is selected

--- app/src/main/java/com/moringa/favoriterecipe/RecipeActivity.java
+++ app/src/main/java/com/moringa/favoriterecipe/RecipeActivity.java
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.BindView;
 
@@ -21,10 +25,20 @@ public class RecipeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
 
-        Intent intent = getIntent();
+
 
         ArrayAdapter adapter=new ArrayAdapter(this, android.R.layout.simple_list_item_1,recipes);
         mRecipesListView.setAdapter(adapter);
 
+        mRecipesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String recipe=((TextView)view).getText().toString();
+                Toast.makeText(RecipeActivity.this,recipe,Toast.LENGTH_LONG).show();
+            }
+        });
+
+        Intent intent = getIntent();
+        String searchedRecipe=intent.getStringExtra("searchedRecipe");
     }
 }
