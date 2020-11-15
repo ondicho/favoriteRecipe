0ba0ca7bcb42754d79337090181fe035f52f14e1
ondicho <ondicho53@gmail.com>
2020-11-16 01:16:36 +0300

    butterknifes the views in the layout in the activity

--- app/src/main/java/com/moringa/favoriterecipe/SearchActivity.java
+++ app/src/main/java/com/moringa/favoriterecipe/SearchActivity.java
@@ -3,12 +3,24 @@ package com.moringa.favoriterecipe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class SearchActivity extends AppCompatActivity {
+import butterknife.BindView;
+
+public class SearchActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.searchRecipe) EditText mSearchRecipeEditText;
+    @BindView(R.id.searchfForRecipe) Button mSearchRecipeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v==m)
     }
 }
