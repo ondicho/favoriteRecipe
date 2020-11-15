689b72dfa63e985ad1a303eb4207a29f9800e1ea
ondicho <ondicho53@gmail.com>
2020-11-16 01:28:51 +0300

    sets an array adapter for the listview in recipe activity

--- app/src/main/java/com/moringa/favoriterecipe/RecipeActivity.java
+++ app/src/main/java/com/moringa/favoriterecipe/RecipeActivity.java
@@ -2,13 +2,28 @@ package com.moringa.favoriterecipe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import butterknife.BindView;
 
 public class RecipeActivity extends AppCompatActivity {
+
+    private ListView mListView;
+    @BindView(R.id.recipesListView) ListView mRecipesListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
+
+        Intent intent = getIntent();
+
+        ArrayAdapter adapter=new ArrayAdapter(this, android.R.layout.simple_list_item_1,recipes);
+        mRecipesListView.setAdapter(adapter);
+
     }
 }
