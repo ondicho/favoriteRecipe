65be78c66294a05dc35d8fb70ef6f9f2f52ae452
ondicho <ondicho53@gmail.com>
2020-11-26 19:33:07 +0300

    creates a shared preference in constants.java

--- app/src/main/java/Constants/Constants.java
+++ app/src/main/java/Constants/Constants.java
@@ -2,4 +2,5 @@ package Constants;
 
 public class Constants {
     public static final String RECIPE_PUPPY_BASE_URL="http://www.recipepuppy.com/api/";
+    public static final String PREFERNCES_SEARCHEDRECIPE_KEY="searchedRecipe";
 }
