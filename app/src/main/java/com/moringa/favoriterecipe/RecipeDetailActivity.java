bbc6cfebff3de6c8dc462fd56402dd1e4620d584
ondicho <ondicho53@gmail.com>
2020-11-26 10:09:12 +0300

    creates a new activity recipeDetailActivity

---
+++ app/src/main/java/com/moringa/favoriterecipe/RecipeDetailActivity.java
@@ -0,0 +1,14 @@
+package com.moringa.favoriterecipe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecipeDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_detail);
+    }
+}
