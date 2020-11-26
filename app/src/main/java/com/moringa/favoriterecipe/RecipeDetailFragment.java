6c4eca728d033845c081a0ddbaf9cbe7f0c0b118
ondicho <ondicho53@gmail.com>
2020-11-26 11:16:59 +0300

    pulls data from Api and sets it in our fragment

--- app/src/main/java/com/moringa/favoriterecipe/RecipeDetailFragment.java
+++ app/src/main/java/com/moringa/favoriterecipe/RecipeDetailFragment.java
@@ -4,12 +4,25 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.Parcel;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.moringa.favoriterecipe.R;
+import models.Result;
+import com.squareup.picasso.Picasso;
+
+
+import org.parceler.Parcels;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link RecipeDetailFragment#newInstance} factory method to
@@ -17,50 +30,49 @@ import android.widget.TextView;
  */
 public class RecipeDetailFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    @BindView(R.id.imageView) ImageView mImageLabel;
+    @BindView(R.id.recipeTitleTextView) TextView mTitleLabel;
+    @BindView(R.id.ingredientsTextView) TextView mIngredientsLabel;
+    @BindView(R.id.thumbnailTextView) TextView mThumbnailLabel;
+    @BindView(R.id.saveRecipeButton) TextView mSaveRecipeButton;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private Result mRecipe;
 
     public RecipeDetailFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RecipeDetailFragment.
-     */
+
     // TODO: Rename and change types and number of parameters
-    public static RecipeDetailFragment newInstance(String param1, String param2) {
-        RecipeDetailFragment fragment = new RecipeDetailFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public static RecipeDetailFragment newInstance(Result recipe) {
+       RecipeDetailFragment reciepeDetailFragment=new RecipeDetailFragment();
+       Bundle args=new Bundle();
+       args.putParcelable("recipe", Parcels.wrap(recipe));
+       reciepeDetailFragment.setArguments(args);
+       return reciepeDetailFragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+        mRecipe=Parcels.unwrap(getArguments().getParcelable("recipe"));
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipe_detail, container, false);
+
+        View view=inflater.inflate(R.layout.fragment_recipe_detail, container, false);
+        ButterKnife.bind(this,view);
+        Picasso.get().load(mRecipe.getHref()).into(mImageLabel);
+
+        List<Result> ingredients=new ArrayList<>();
+
+        mTitleLabel.setText(mRecipe.getTitle());
+        mIngredientsLabel.setText(mRecipe.getIngredients());
+        mThumbnailLabel.setText(mRecipe.getThumbnail());
+
+        return view;
     }
 }
