88980f6706b26861be39e6ff38104335872814a7
ondicho <ondicho53@gmail.com>
2020-12-01 12:19:47 +0300

    sets an onclick listener on the signup text view in the login activity

--- app/src/main/java/com/moringa/favoriterecipe/LoginActivity.java
+++ app/src/main/java/com/moringa/favoriterecipe/LoginActivity.java
@@ -2,13 +2,32 @@ package com.moringa.favoriterecipe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @BindView(R.id.signUpTextView) TextView mSignUpTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ButterKnife.bind(this);
+        mSignUpTextView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v==mSignUpTextView){
+            Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
