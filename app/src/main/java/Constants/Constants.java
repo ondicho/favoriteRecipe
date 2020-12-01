18cb9199d4e5e40840a99caceb5e09955a1b3ba6
ondicho <ondicho53@gmail.com>
2020-12-01 12:39:22 +0300

    writes a function to save searchedrecipe to firebase

--- app/src/main/java/Constants/Constants.java
+++ app/src/main/java/Constants/Constants.java
@@ -3,4 +3,5 @@ package Constants;
 public class Constants {
     public static final String RECIPE_PUPPY_BASE_URL="http://www.recipepuppy.com/api/";
     public static final String PREFERNCES_SEARCHEDRECIPE_KEY="searchedRecipe";
+    public static final String FIREBASE_CHILD_SEARCHED_RECIPE= "searchedRecipe";
 }
--- app/src/main/java/com/moringa/favoriterecipe/SearchActivity.java
+++ app/src/main/java/com/moringa/favoriterecipe/SearchActivity.java
@@ -12,27 +12,35 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class SearchActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private SharedPreferences mSharedPreferences;
-    private SharedPreferences.Editor mEditor;
+//    private SharedPreferences mSharedPreferences;
+//    private SharedPreferences.Editor mEditor;
 
+    private DatabaseReference mSearchedRecipeReference;
     @BindView(R.id.searchRecipe) EditText mSearchRecipeEditText;
     @BindView(R.id.searchfForRecipe) Button mSearchRecipeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        mSearchedRecipeReference= FirebaseDatabase.getInstance()
+                .getReference().child(Constants.FIREBASE_CHILD_SEARCHED_RECIPE);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
         ButterKnife.bind(this);
 
         Intent intent = getIntent();
 
-        mSharedPreferences= PreferenceManager.getDefaultSharedPreferences(this);
-        mEditor=mSharedPreferences.edit();
+//        mSharedPreferences= PreferenceManager.getDefaultSharedPreferences(this);
+//        mEditor=mSharedPreferences.edit();
 
         mSearchRecipeButton.setOnClickListener(this);
     }
@@ -40,14 +48,19 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
             public void onClick(View v) {
                 if (v == mSearchRecipeButton) {
                     String searchedRecipe = mSearchRecipeEditText.getText().toString();
-                    addToSharedPreferences(searchedRecipe);
+                    saveRecipeToFirebase(searchedRecipe);
+//                    addToSharedPreferences(searchedRecipe);
                     Intent intent = new Intent(SearchActivity.this, RecipeActivity.class);
                     intent.putExtra("searchedRecipe", searchedRecipe);
                     startActivity(intent);
                 }
             }
-
-    private void addToSharedPreferences(String searchedRecipe) {
-        mEditor.putString(Constants.PREFERNCES_SEARCHEDRECIPE_KEY, searchedRecipe).apply();
+    public void saveRecipeToFirebase(String searchedRecipe) {
+        mSearchedRecipeReference.push().setValue(searchedRecipe);
     }
+
+
+//    private void addToSharedPreferences(String searchedRecipe) {
+//        mEditor.putString(Constants.PREFERNCES_SEARCHEDRECIPE_KEY, searchedRecipe).apply();
+//    }
 }
