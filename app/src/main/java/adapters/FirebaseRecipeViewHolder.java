102ed1371024c1c482677947659a9e339989951c
ondicho <ondicho53@gmail.com>
2020-12-02 06:57:04 +0300

    implements user account creation using firebase

--- app/src/main/java/adapters/FirebaseRecipeViewHolder.java
+++ app/src/main/java/adapters/FirebaseRecipeViewHolder.java
@@ -1,4 +1,24 @@
 package adapters;
 
-public class FirebaseRecipeViewHolder {
+import android.content.Context;
+import android.view.View;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class FirebaseRecipeViewHolder  extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+    View mView;
+    Context mContext;
+
+    public FirebaseRecipeViewHolder(View itemView){
+        super(itemView);
+        mView=itemView;
+        mContext=itemView.getContext();
+        itemView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
 }
