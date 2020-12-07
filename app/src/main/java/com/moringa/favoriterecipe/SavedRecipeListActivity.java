c8acaf8cfc15cfb07c6ee0ebb60a7a033eb5232a
ondicho <ondicho53@gmail.com>
2020-12-08 02:40:23 +0300

    rewrites savedRecipeList activity to display a list of saved Recipes

--- app/src/main/java/com/moringa/favoriterecipe/SavedRecipeListActivity.java
+++ app/src/main/java/com/moringa/favoriterecipe/SavedRecipeListActivity.java
@@ -1,14 +1,79 @@
 package com.moringa.favoriterecipe;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import Constants.Constants;
+import adapters.FirebaseRecipeViewHolder;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import models.Result;
 
 public class SavedRecipeListActivity extends AppCompatActivity {
+    private DatabaseReference mRecipeReference;
+    private FirebaseRecyclerAdapter<Result, FirebaseRecipeViewHolder> mFirebaseAdapter;
+
+
+    @BindView(R.id.recyclerView) RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_saved_recipe_list);
+        setContentView(R.layout.activity_recipe);
+        ButterKnife.bind(this);
+
+        mRecipeReference = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_RECIPE  );
+        setUpFirebaseAdapter();
     }
+
+    private void setUpFirebaseAdapter(){
+        FirebaseRecyclerOptions<Result> options =
+                new FirebaseRecyclerOptions.Builder<Result>()
+                        .setQuery(mRecipeReference, Result.class)
+                        .build();
+
+        mFirebaseAdapter = new FirebaseRecyclerAdapter<Result, FirebaseRecipeViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull FirebaseRecipeViewHolder firebaseRestaurantViewHolder, int position, @NonNull Result recipe) {
+                firebaseRestaurantViewHolder.bindRecipe(recipe);
+            }
+
+            @NonNull
+            @Override
+            public FirebaseRecipeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recipe_list_item, parent, false);
+                return new FirebaseRecipeViewHolder(view);
+            }
+        };
+
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(mFirebaseAdapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mFirebaseAdapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mFirebaseAdapter!= null) {
+            mFirebaseAdapter.stopListening();
+        }
+    }
+}
 }
