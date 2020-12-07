7d018a9fa03076c63b2209255e5b7fee9dd11858
ondicho <ondicho53@gmail.com>
2020-12-08 01:10:36 +0300

    adds logout functionality in the main activity

--- app/src/main/java/com/moringa/favoriterecipe/MainActivity.java
+++ app/src/main/java/com/moringa/favoriterecipe/MainActivity.java
@@ -6,8 +6,13 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -31,6 +36,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mViewSavedRecipesButton.setOnClickListener(this);
     }
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_logout) {
+            logout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     public void onClick(View v) {
         if (v == mFindRecipeButton) {
             Intent intent = new Intent(MainActivity.this, RecipeActivity.class);
@@ -40,6 +62,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 //            Intent intent = new Intent(MainActivity.this, SavedRListActivity.class);
 //            startActivity(intent);
 //        }
+    }
+
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
     }
 
 }
