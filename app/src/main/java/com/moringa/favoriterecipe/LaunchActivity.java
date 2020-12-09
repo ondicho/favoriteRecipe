a9655ebbd0efa4692baa0e0777907e53d1e9ecb3
ondicho <ondicho53@gmail.com>
2020-12-09 06:47:44 +0300

    designs the launch_activity xml file

--- app/src/main/java/com/moringa/favoriterecipe/LaunchActivity.java
+++ app/src/main/java/com/moringa/favoriterecipe/LaunchActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class LaunchActivi extends AppCompatActivity {
+public class LaunchActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
